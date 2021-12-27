36c91485f2d10bfc59b40588e97b4ca109a4fee7
Vaibhav Minde <vaibhav.minde@kristasoft.com>
2021-12-27 23:10:16 +0530

    CSS done

--- my-app/src/routes/index.svelte
+++ my-app/src/routes/index.svelte
@@ -4,11 +4,44 @@
     const title = "Todo App"
 </script>
 
-<h1> {title} </h1>
+<style>
+    .todos {
+        width: 100%;
+        max-width: 42rem;
+        margin: 3rem auto 0 auto;
+    }
 
-<form action="" method="">
-    <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo">
+    .new {
+        margin: 0 0 0.5rem 0;
+    }
 
-</form>
+    .new input {
+        font-size: 20px;
+        width: 100%;
+        padding: 0.5em 1em 0.3em 1em;
+        box-sizing: border-box;
+        background: rgba(255, 255, 255, 0.05);
+        border-radius: 8px;
+        text-align: center;
+    }
 
-<TodoItem/>
+    .todos :global(input:focus-visible) {
+        box-shadow: inset 1px 1px 6px rgba(0, 0, 0, 0.1);
+        border: 1px solid #ff3c00 !important;
+        outline: none;
+    }
+</style>
+
+<div class="todos">
+    <h1> {title} </h1>
+
+    <form action="" method="" class="new">
+        <input type="text" name="text" aria-label="Add a todo" placeholder="+ type to add a todo">
+
+    </form>
+
+    <TodoItem/>
+    <TodoItem/>
+    <TodoItem/>
+</div>
+
